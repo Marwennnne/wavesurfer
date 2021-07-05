1c4ee5f844de57c008ecc6a6ffc0e5fd4fec43cf
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-07-05 20:56:13 +0100

    divideAndConquer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import Region from 'wavesurfer.js/dist/plugin/wavesurfer.regions.js';
 })
 export class AppComponent implements OnInit {
   regions: any;
-  entries: any = [];
+  entries:any;
   ngOnInit(): void {
     var wavesurfer = WaveSurfer.create({
       container: '#waveform',
@@ -30,15 +30,53 @@ export class AppComponent implements OnInit {
     wavesurfer.on('region-update-end', () => {
       this.regions = wavesurfer.regions.list;
       this.entries = Object.entries(this.regions);
-      this.checkTri();
-      
+      console.log(this.entries)
+      this.quickSort(this.entries)
     });
+
 
 
   }
   title = 'new-project';
 
-  checkTri(){
-    this.entries.sort((a,b) => a[1].start - b[1].start);
-  }
+  quickSort = (arr, left = 0, right = arr.length - 1) => {
+    let len = arr.length, index;
+    if(len > 1) {
+       index = this.partition(arr, left, right)
+       if(left < index - 1) {
+          this.quickSort(arr, left, index - 1)
+       }
+       if(index < right) {
+          this.quickSort(arr, index, right)
+       }
+    }
+    return arr
+ }
+    partition = (arr, left, right) => {
+    let middle = Math.floor((right + left) / 2),
+    pivot = arr[middle],
+    i = left, // Start pointer at the first item in the
+
+    j = right // Start pointer at the last item in the array
+    while(i <= j) {
+       // Move left pointer to the right until the value at the
+       // left is greater than the pivot value
+       while(arr[i][1].start < pivot[1].start) {
+          i++
+       }
+       // Move right pointer to the left until the value at the
+       // right is less than the pivot value
+       while(arr[j][1].start > pivot[1].start) {
+          j--
+       }
+       // If the left pointer is less than or equal to the
+       // right pointer, then swap values
+       if(i <= j) {
+          [arr[i], arr[j]] = [arr[j], arr[i]] // ES6 destructuring swap
+          i++
+          j--
+       }
+    }
+    return i
+ }
 }
